86ebbcdb0a127889801b8697f3aba99e6f8387c0
Nick Dichev <github@dichev.email>
2026-01-22 12:07:36 -0800

    Rename command to search-portal-wholesale-brands

--- raycast-env.d.ts
+++ raycast-env.d.ts
@@ -13,12 +13,12 @@ type ExtensionPreferences = {}
 declare type Preferences = ExtensionPreferences
 
 declare namespace Preferences {
-  /** Preferences accessible in the `search-brands` command */
-  export type SearchBrands = ExtensionPreferences & {}
+  /** Preferences accessible in the `search-portal-wholesale-brands` command */
+  export type SearchPortalWholesaleBrands = ExtensionPreferences & {}
 }
 
 declare namespace Arguments {
-  /** Arguments passed to the `search-brands` command */
-  export type SearchBrands = {}
+  /** Arguments passed to the `search-portal-wholesale-brands` command */
+  export type SearchPortalWholesaleBrands = {}
 }
 
